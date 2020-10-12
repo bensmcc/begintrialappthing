79ee445d0043c03d91779296e1ac86a47bca8308
bensmcc <bensmcc@gmail.com>
2020-10-11 21:27:42 -0400

    Update index.ts

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -16,7 +16,7 @@ let body = `
     </h1>
 
     <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
+      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go! Does this work?
     </p>
 
   </body>
